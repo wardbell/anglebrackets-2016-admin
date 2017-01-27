dfdd2174dd15ed8e375c1c8cf72eb70dd6b8ba79
Ward Bell <wardbell@hotmail.com>
2017-01-26 21:53:37 -0800

    feature: colors and comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFire } from 'angularfire2';
-import { Observable } from 'rxjs/Observable';
 
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/of';
@@ -9,16 +9,21 @@ import 'rxjs/add/observable/of';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:  ['./app.component.css']
 })
 export class AppComponent {
   title = 'Admins';
-  uid: Observable<string>;
+
+  uid:     Observable<string>;
   isAdmin: Observable<boolean>;
 
   constructor(public af: AngularFire) {
+
+    // AngularFire.auth is an Observable<FirebaseAuthState>
+    // Update UI when auth-state changes
+    // uid is an Observable<string>
     this.uid = this.af.auth.map(state => {
-      if(state) {
+      if (state) {
         return state.google.uid;
       } else {
         return null;
@@ -26,20 +31,28 @@ export class AppComponent {
     });
 
 
+    // When auth-state changes
+    // request info about the current user.
+    // which is an async DB call that returns an Observable<admin-user-profile>
     this.isAdmin = this.af.auth.switchMap(state => {
-      if(state) {
-        let uid = state.google.uid;
+      if (state) {
+        const uid = state.google.uid;
+
+        // if the user-profile.value is truthy, this person is an Admin
+        // watch for change to `value` as person might not stay an admin
         return this.af.database.object('/admins/' + uid)
-        .map(obj => obj.$value == true);
+          .map(obj => !!obj.$value);
+
       } else {
         return Observable.of(false);
       }
-    })
+    });
   }
 
   login() {
     this.af.auth.login();
   }
+
   logout() {
     this.af.auth.logout();
   }
